57ec9741c2444d7adaf5c9676fc17bb2a4fc521a
Ian Villanueva <ian.villanueva@thesalesmachine.com>
2024-02-13 15:53:23 -0500

    first commit

---
+++ template.js
@@ -0,0 +1,15 @@
+export default () => {
+	return `<doctype html>
+	<html lang="en">
+		<head>
+			<meta charset="utf-8">
+			<title>MERN Skeletont</title>
+		</head>
+		<body>
+			<div id="root">
+				Is it working?
+			</div>
+		
+		</body>
+	</html>`
+};
